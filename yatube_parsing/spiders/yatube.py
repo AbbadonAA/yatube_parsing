5c7dbd42c84e08e5fd28152446363de3ef0998da
Pushkarev Anton <pushkarevantona@gmail.com>
2022-08-13 23:38:39 +0300

    Initial

--- yatube_parsing/spiders/yatube.py
+++ yatube_parsing/spiders/yatube.py
@@ -7,4 +7,12 @@ class YatubeSpider(scrapy.Spider):
     start_urls = ['http://51.250.32.185/']
 
     def parse(self, response):
-        pass
+        for citation in response.css('div.card-body'):
+            yield {
+                'author': citation.css('a strong::text').get(),
+                'text': ''.join(citation.css('p::text').getall()).strip(),
+                'date': citation.css('small.text-muted::text').get(),
+            }
+        next_page = response.css('a:contains("Следующая")::attr(href)').get()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
